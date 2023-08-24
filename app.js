9e891f883ce2773ecd13e0ee91bb010915aa8e2c
Daniel Yan <felzyan33@gmail.com>
2023-08-24 21:46:31 +0800

    Fix: support resizing from (top, left, bottom, right)

--- app.js
+++ app.js
@@ -61,6 +61,22 @@ window.onload = function () {
             }
         }).resizable({
             edges: { left: true, right: true, bottom: true, top: true },
+            listeners: {
+                move: function(event) {
+                  let { x, y } = event.target.dataset
+        
+                  x = (parseFloat(x) || 0) + event.deltaRect.left
+                  y = (parseFloat(y) || 0) + event.deltaRect.top
+        
+                  Object.assign(event.target.style, {
+                    width: `${event.rect.width}px`,
+                    height: `${event.rect.height}px`,
+                    transform: `translate(${x}px, ${y}px)`
+                  })
+        
+                  Object.assign(event.target.dataset, { x, y })
+                }
+            },
             margin: 10
         });
 
