c3c5af9e2390e5177108823008c792ee6c1ce170
Daniel Yan <felzyan33@gmail.com>
2023-08-24 23:07:08 +0800

    Format code

--- app.js
+++ app.js
@@ -68,19 +68,19 @@ window.onload = function () {
                     event.target.style.zIndex = ++zIndex;
                 },
                 move: function(event) {
-                  let { x, y } = event.target.dataset
+                    let { x, y } = event.target.dataset;
         
-                  x = (parseFloat(x) || 0) + event.deltaRect.left
-                  y = (parseFloat(y) || 0) + event.deltaRect.top
+                    x = (parseFloat(x) || 0) + event.deltaRect.left;
+                    y = (parseFloat(y) || 0) + event.deltaRect.top;
         
-                  Object.assign(event.target.style, {
-                      width: `${event.rect.width}px`,
-                      height: `${event.rect.height}px`,
-                      transform: `translate(${x}px, ${y}px)`
-                  })
-                  Object.assign(event.target.dataset, { x, y });
-                  note.x = x;
-                  note.y = y;
+                    Object.assign(event.target.style, {
+                        width: `${event.rect.width}px`,
+                        height: `${event.rect.height}px`,
+                        transform: `translate(${x}px, ${y}px)`
+                    });
+                    Object.assign(event.target.dataset, { x, y });
+                    note.x = x;
+                    note.y = y;
                 },
                 end: function(event) {
                     enableScroll();
