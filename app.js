5434d6bb8e0f7c8e8ec9f63229c29da731233f70
Daniel Yan <felzyan33@gmail.com>
2023-08-24 23:00:59 +0800

    Format code

--- app.js
+++ app.js
@@ -74,9 +74,9 @@ window.onload = function () {
                   y = (parseFloat(y) || 0) + event.deltaRect.top
         
                   Object.assign(event.target.style, {
-                    width: `${event.rect.width}px`,
-                    height: `${event.rect.height}px`,
-                    transform: `translate(${x}px, ${y}px)`
+                      width: `${event.rect.width}px`,
+                      height: `${event.rect.height}px`,
+                      transform: `translate(${x}px, ${y}px)`
                   })
                   Object.assign(event.target.dataset, { x, y });
                   note.x = x;
